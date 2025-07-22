512f960f2b2e63796d3028c9cf7c31c4a6bd93db
tha_scarfaze <thascarfaze@gmail.com>
2025-07-23 01:27:22 +0400

    update pixabay-api

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,21 @@
 import axios from 'axios';
 
+const API_KEY = '51410933-6e28737e6449aa5e8ee5fd880';
+const BASE_URL = 'https://pixabay.com/api/';
+const PER_PAGE = 15;
 
-export const getImagesByQuery = query => {
-  return axios
-    .get('https://pixabay.com/api/', {
-      params: {
-        key: '51410933-6e28737e6449aa5e8ee5fd880',
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-      },
-    })
-    .then(response => response.data);
+export const getImagesByQuery = async (query, page = 1) => {
+  const response = await axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: PER_PAGE,
+      page: page,
+    },
+  });
+
+  return response.data;
 };
